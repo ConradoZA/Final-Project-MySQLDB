9df280976dafb51007826f5fa17cdd7755d40904
Conrado <zaolen@gmail.com>
2020-06-19 22:35:33 +0200

    actually restricting origin acces

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 
-header("Access-Control-Allow-Origin: *");
+define('LARAVEL_START', microtime(true));
+
+header("Access-Control-Allow-Origin: https://play2games.herokuapp.com");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
@@ -8,8 +10,6 @@ $method = $_SERVER['REQUEST_METHOD'];
 if($method === "OPTIONS") {
     die();
 }
-
-define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
