806cd538f0a26769a1e2dd43da1a8b3b9f6c8f3c
Conrado <zaolen@gmail.com>
2020-06-19 22:07:31 +0200

    never use a == in php

--- public/index.php
+++ public/index.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Allow: GET, POST, OPTIONS, PUT, DELETE");
 $method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
+if($method === "OPTIONS") {
     die();
 }
 
